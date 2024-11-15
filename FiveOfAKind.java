c888af9567564fdc50963792667ad7c5538e8b94
jklopson <jaklopson@gmail.com>
2024-11-14 21:20:19 -0500

    lower section scores implementation

---
+++ FiveOfAKind.java
@@ -0,0 +1,12 @@
+//Jack Klopson | CS2100 Final Project FiveOfAKind Class | 11-14-2024
+public class FiveOfAKind extends Category {
+    /**
+     * evaluate abstract method implementation
+     * for three of a kind
+     * @return score based on a yahtzee!
+     */
+    @Override
+    public int evaluate(Dice d) {
+        return 50;
+    }
+}
---
+++ FullHouse.java
@@ -0,0 +1,12 @@
+//Jack Klopson | CS2100 Final Project FullHouse Class | 11-14-2024
+public class FullHouse extends Category {
+    /**
+     * evaluate abstract method implementation
+     * for three of a kind
+     * @return score based on a full house
+     */
+    @Override
+    public int evaluate(Dice d) {
+        return 25;
+    }
+}
