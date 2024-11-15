9b4509cca6b33c30b2e45f166cbf07727cd63f4c
jklopson <jaklopson@gmail.com>
2024-11-14 19:54:49 -0500

    Die.java constructor, roll, getValue, toString

---
+++ Die.java
@@ -0,0 +1,42 @@
+//Jack Klopson | Final Project Die Class | 11-14-2024
+
+import java.util.Random;
+
+public class Die {
+    private static final int SIDES = 6;
+    private int value;
+    private Random r = new Random();
+    /**
+     * Default Constructor for Die object
+     */
+    public Die(){
+        value = r.nextInt(1, SIDES+1);
+    }
+
+    /**
+     * Setter method for the value of a
+     * Die object. Sets the value to 
+     */
+    public void roll() {
+        value = r.nextInt(1, SIDES+1);
+    }
+
+    /**
+     * Getter method for the value of the Die
+     * @return value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * Overridden toString method that returns the
+     * value of the Die instead of a memory address
+     * @return String format of the value of the Die
+     */
+    @Override
+    public String toString() {
+        return ""+value;
+    }
+}
+
